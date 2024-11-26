5e61ddc9d863641166e0c0478d1c4340f6e46718
flair <fernando_punk77@hotmail.com>
2024-11-26 11:21:06 -0300

    arreglo del fetch

--- khai-frontend/src/components/Table/Table.jsx
+++ khai-frontend/src/components/Table/Table.jsx
@@ -1,25 +1,64 @@
-import UseFetch from "../UseFetch/useFetch";
+import { useState, useEffect } from "react";
+import "./styles.css";
 
 const Table = () => {
 
     let url = "http://localhost:8081/"
-
-    const data = UseFetch(url);
-    const dataImports = data.imports;
-    console.log(dataImports);
     
+    useEffect( () =>{
+        getData(url);
+    },[url])
+
+    const [data, setData] = useState([]); 
+
+    const getData = () => {
+        fetch(url).then(response => response.json())
+        .then(data => {
+        const dataImports = data.imports;
+        setData(dataImports);
+        })
+        .catch(error => console.log(error))
+    }
+        
     return (
         <>
             <div>
-            {
-                dataImports.map( p =>(  
+            <table>
+                <thead>
+                    <tr>
+                        <th>BL</th>
+                        <th>Origen</th>
+                        <th>Producto</th>
+                        <th>E.T.A.</th>
+                        <th>Condicion</th>
+                        <th>Buque</th>
+                        <th>Cliente</th>
+                        <th>Consignatario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map(p => (
+                            <tr key={p.id}>
+                                <td>{p.bl}</td>
+                                <td>{p.origen}</td>
+                                <td>{p.product}</td>
+                                <td>{p.arrival}</td>
+                                <td>{p.condition}</td>
+                                <td>{p.buque}</td>
+                                <td>{p.shipper}</td>
+                                <td>{p.consignee}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            {/* {
+                data.map( p =>(  
                     <div key={p.id}>
-                        <h5>{p.bl}</h5>
-                        <p>{p.origen}</p>
-                        <p>{p.producto}</p>
                     </div>                      
                 ))
-            }
+            } */}
             </div>
         </>
     )
